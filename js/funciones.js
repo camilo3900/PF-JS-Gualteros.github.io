ca3b919f1158bd035adcef23444abf40ff08a254
camilo3900 <camilogv.1629@gmail.com>
2023-07-02 18:10:03 -0500

    Se el array por un archivo .json

--- js/funciones.js
+++ js/funciones.js
@@ -7,334 +7,112 @@ class Pelicula {
     this.img = img;
     this.genero = genero;
   }
-  alerta() {
-    alert("Su nombre es: " + this.nombre + "\nSu precio es: $" + this.precio);
+  pagar(cantidad) {
+    return total= this.precio*cantidad;
   }
 }
-
-/* Clase de tipo Dulceria */
-class Comida {
-  constructor(id, nombre, precio, img) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.img = img;
-  }
-  descripcion(d1, d2, d3) {
-    console.log("Detalle 1: " + d1);
-    console.log("Detalle 2: " + d2);
-    console.log("Detalle 3: " + d3);
-  }
+/* Funcion para consumir archivo json de peliculas */
+function consumirPeliculas(){
+  fetch('../peliculas.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    pelis=data;
+    console.log(pelis);
+    llenarLocalStorage('peliculasLS', pelis ); 
+  })
+  .catch(function(error) {
+    console.log('Ocurrió un error:'+error);
+  });
 }
-
-/* Objetos Comida */
-const comida1 = new Comida(1, "Combo 1", 37000, "../images/combo1.jpg");
-comida1.descripcion(
-  "1 Crispeta Grande",
-  "2 Gaseosas Grandes 32 Oz",
-  "2 Chocolatinas 12 gr"
-);
-const comida2 = new Comida(2, "Combo 2", 54000, "../images/combo1.jpg");
-comida2.descripcion(
-  "1 Crispeta Mediana",
-  "2 Perros Calientes",
-  "2 Gaseosas Medianas Vaso 22 Oz"
-);
-const comida3 = new Comida(3, "Combo 3", 25000, "../images/combo1.jpg");
-comida3.descripcion(
-  "1 Crispeta Mediana",
-  "1 Gaseosa Mediana Vaso 22 Oz",
-  "1 Chocolatina 12 gr"
-);
-const comida4 = new Comida(4, "Combo 4", 22000, "../images/combo1.jpg");
-comida4.descripcion(
-  "1 Perro Caliente",
-  "1 Gaseosa Mediana Vaso 22 Oz",
-  "1 Chocolatina 12 gr"
-);
-const comida5 = new Comida(5, "Combo 5", 45000, "../images/combo1.jpg");
-comida5.descripcion(
-  "1 Crispeta Mediana",
-  "2 Gaseosas Medianas Vaso 22 Oz",
-  "1 Perro Caliente"
-);
-/* Objetos tipo Pelicula */
-
-const pel1 = new Pelicula(
-  1,
-  "Rapido y furioso 10",
-  9000,
-  "../images/pelis/rapidos-&-furiosos.jpg",
-  "Accion"
-);
-const pel2 = new Pelicula(
-  2,
-  "Star Wars",
-  7600,
-  "../images/pelis/star-wars.jpg",
-  "Accion"
-);
-const pel3 = new Pelicula(
-  3,
-  "Creed III",
-  2800,
-  "../images/pelis/creed-iii.jpg",
-  "Accion"
-);
-const pel4 = new Pelicula(
-  4,
-  "Navalny",
-  1500,
-  "../images/pelis/navalny.jpg",
-  "Guerra"
-);
-const pel5 = new Pelicula(
-  5,
-  "Nine Bullets",
-  5800,
-  "../images/pelis/nine_bullets.jpg",
-  "Accion"
-);
-const pel6 = new Pelicula(
-  6,
-  "Trasformers: El origen de las bestias",
-  1200,
-  "../images/pelis/transformers-rise-of-the-beast.jpg",
-  "Accion"
-);
-const pel7 = new Pelicula(
-  7,
-  "La  Bestia",
-  1800,
-  "../images/pelis/beast.jpg",
-  "Accion"
-);
-const pel8 = new Pelicula(
-  8,
-  "La Maldición",
-  2800,
-  "../images/pelis/the-accursed.jpg",
-  "Terror"
-);
-const pel9 = new Pelicula(
-  9,
-  "Avatar Bajo el Agua",
-  57000,
-  "../images/pelis/avatar-the-way-of-water.jpg",
-  "Aventura"
-);
-const pel10 = new Pelicula(
-  10,
-  "Todo sobre mi madre",
-  4000,
-  "../images/pelis/beast.jpg",
-  "Accion"
-);
-const pel11 = new Pelicula(
-  11,
-  "Briuser",
-  5500,
-  "../images/pelis/bruiser.jpg",
-  "Accion"
-);
-const pel12 = new Pelicula(
-  12,
-  "Emancipation",
-  7000,
-  "../images/pelis/emancipation.jpg",
-  "Accion"
-);
-const pel13 = new Pelicula(
-  13,
-  "Guardianes de la Glaxia V.03",
-  9000,
-  "../images/pelis/guardians-of-the-galaxy.jpg",
-  "Accion"
-);
-const pel14 = new Pelicula(
-  14,
-  "Sonic: The Hedgegod",
-  8000,
-  "../images/pelis/sonic-the-hedgehog.jpg",
-  "Infantil"
-);
-const pel15 = new Pelicula(
-  15,
-  "Flash",
-  6700,
-  "../images/pelis/flash.jpg",
-  "Accion"
-);
-const pel16 = new Pelicula(
-  16,
-  "Mario Bros: La pelicula",
-  11000,
-  "../images/pelis/super-mario-bros-the-movie.jpg",
-  "Aventura"
-);
-const pel17 = new Pelicula(
-  17,
-  "Elemental",
-  5500,
-  "../images/pelis/elemental.jpg",
-  "Infantil"
-);
-const pel18 = new Pelicula(
-  18,
-  "End of Loyalty",
-  6000,
-  "../images/pelis/end-of-loyalty.jpg",
-  "Guerra"
-);
-const pel19 = new Pelicula(
-  19,
-  "Mansion Encantada",
-  9000,
-  "../images/pelis/haunted-mansion.jpg",
-  "Terror"
-);
-const pel20 = new Pelicula(
-  20,
-  "Peter Pan y Wendy",
-  8000,
-  "../images/pelis/peter-pan-y-wendy.jpg",
-  "Infantil"
-);
-const pel21 = new Pelicula(
-  21,
-  "Spiderman: Across the spiderverse",
-  8000,
-  "../images/pelis/spiderman_across_the_spiderverse.jpg",
-  "Aventura"
-);
-const pel22 = new Pelicula(
-  22,
-  "Paw Patrol: The migthy movie",
-  8000,
-  "../images/pelis/paw-patrol-the-mighty-movie.jpg",
-  "Infantil"
-);
-const pel23 = new Pelicula(
-  23,
-  "Indiana Jones: El Dia del Destino",
-  8000,
-  "../images/pelis/indiana-jones-and-the-dial-of-destiny.jpg",
-  "Aventura"
-);
-const pel24 = new Pelicula(
-  24,
-  "Extraction 2",
-  8000,
-  "../images/pelis/extraction-two.jpg",
-  "Guerra"
-);
-const pel25 = new Pelicula(
-  25,
-  "Chicken Run",
-  8000,
-  "../images/pelis/chicken-run.jpg",
-  "Infantil"
-);
-const pel26 = new Pelicula(
-  26,
-  "Bad Guys",
-  8000,
-  "../images/pelis/bad-guys.jpg",
-  "Infantil"
-);
-const pel27 = new Pelicula(
-  27,
-  "La Elefanta del Mago",
-  8000,
-  "../images/pelis/magicians-elephant.jpg",
-  "Aventura"
-);
-const pel28 = new Pelicula(
-  28,
-  "Scream Six",
-  8000,
-  "../images/pelis/scream-six.jpg",
-  "Terror"
-);
-const pel29 = new Pelicula(
-  29,
-  "Evil Dead Rise",
-  8000,
-  "../images/pelis/evil-dead-rise.jpg",
-  "Terror"
-);
-const pel30 = new Pelicula(
-  30,
-  "Contractor",
-  8000,
-  "../images/pelis/contractor.jpg",
-  "Guerra"
-);
-const pel31 = new Pelicula(
-  31,
-  "Adam Project",
-  8000,
-  "../images/pelis/adam-project.jpg",
-  "Guerra"
-);
-const pel32 = new Pelicula(
-  32,
-  "Amazing Maurice",
-  8000,
-  "../images/pelis/amazing-maurice.jpg",
-  "Infantil"
-);
-const pel33 = new Pelicula(
-  33,
-  "The boogeyman",
-  8000,
-  "../images/pelis/boogeyman.jpg",
-  "Terror"
-);
-const pel34 = new Pelicula(
-  34,
-  "Living with Chucky",
-  8000,
-  "../images/pelis/living-with-chucky.jpg",
-  "Infantil"
-);
-const pel35 = new Pelicula(
-  35,
-  "Dungeons and Dragons",
-  8000,
-  "../images/pelis/dungeons-and-dragons-honor-among-thieves.jpg",
-  "Aventura"
-);
-const pel36 = new Pelicula(
-  36, 
-  "Antman: Quantumania",
-  12000,
-  "../images/pelis/antman-and-the-wasp-quantumania.jpg",
-  "Accion"
-)
-
-
  /* funcion para llenar el storage */
- function llenarLocalStorage(){
-  localStorage.setItem('pelis', JSON.stringify(peliculas));/* Se convierte el listado de objetos a String */
-  localStorage.setItem('food', JSON.stringify(comidas));
+ function llenarLocalStorage(clave, dato){
+  localStorage.setItem(clave, JSON.stringify(dato));/* Se convierte el listado de objetos a String */
  } 
+/* Funcion para traer las peliculas del localStorage*/
+const peliculaDesdeLS=(llave)=>JSON.parse(localStorage.getItem(llave));/* Se llama cada lista */
 
- /* Funcion para crear card de peliculas */
+
+
+/* Mediante la funcion filtrarCategoria se filtran las peliculas del storage que coincidan con atributo genero escogido */
+let buscaCategoria=function filtrarCategoria(arr, filtro) {
+  const filtrado = arr.filter((el) => {
+    return el.genero==filtro;
+  });
+  return filtrado;
+}
+/* Funcion para crear el contenedor donde se mostrará cada película */
 function crearContenedor(dato){
   const productArticle = document.createElement('article');
   productArticle.innerHTML=
   `<div class="card d-flex flex-fill" >
-  <img src="${dato.img}" alt="${dato.nombre}">
+  <img src=" ${dato.img}" alt="${dato.nombre}">
   <h3>${dato.nombre}</h3>
-  <p class="texto_card">Precio: $${dato.precio} </p>
-  <p class="texto_card">Genero: ${dato.genero} </p>
+  <p>Precio: $${dato.precio} </p>
+  <p>Categoria: ${dato.genero} </p>
     <div class="card-action">
-      <button class="btn_comprar">Comprar</button>
+      <button class="btn_comprar" id="${dato.id}">Comprar</button>
     </div>
 </div>`;
 return productArticle;
 }
+function crearItemCompra(dato){
+  const item = document.createElement('div');
+  item.id="grid-item-carrito";
+  item.innerHTML=`               
+   <img src="${dato.img}" alt="">
+  <div class="carrito-nombre">
+      <h5>Nombre</h5>
+      <h4>${dato.nombre}</h4>
+  </div>
+  <div class="carrito-cantidad">
+      <h5>Cantidad</h5>
+      <h4>1</h4>
+  </div>
+  <div class="carrito-total">
+      <h5>Total</h5>
+      <h4>${dato.precio}</h4>
+  </div>
+  <div class="carrito-subtotal">
+      <h5>Subtotal</h5>
+      <h4>$1000</h4>
+  </div>
+  <div id="opciones_carrito">
+  <button class="carrito-eliminar ${dato.id}"><i class="fa-solid fa-trash"></i></button>
+  <button class="carrito-editar ${dato.id}"><i class="fa-sharp fa-solid fa-pen-to-square"></i></button>
+  </div>`
+  
 
+  return item;
+}
+
+
+
+
+const imagenCarga= ()=>{
+  const imagen= document.createElement('div');
+  imagen.innerHTML=` <div>
+  <img src="https://cdn.pixabay.com/animation/2022/12/05/15/23/15-23-06-837_512.gif" alt="carga">
+  <p>Procesando informacion...</p>
+</div>`
+return imagen;
+}
+let carga = (response, dato, cargar)=>{
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      if(response){
+        resolve("muestra carga");
+        contenedor.appendChild(crearContenedor(dato));
+        contenedor.removeChild(cargar);
+        botonesComprar(); 
+      }
+      else{
+        reject("Sin peliculas");
+      }
+    }, 2000);
+    });
+
+};
 
   
--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-/* const contenedor= document.querySelector('#principal'); */
+/* Llamado de contenedores y declaracion de variables */
+const contenedor = document.querySelector('#principal');
 const buttonCategorias = document.querySelector(".dropdown-menu");
+const contenedorCarrito= document.querySelector('#grid-container-carrito')
+const buscar= document.querySelector('#boton_buscar');
+const inputBusqueda= document.querySelector('#campo');
 const boton=document.querySelector(".btn");
-const contenedor= document.querySelector('#principal');
-const buscar= document.querySelector('#boton_buscar')
-const inputBusqueda= document.querySelector('#campo')
-/* const informacion= document.querySelector('.Info'); */
-/* Se organizan los objetos tipo  */
-const comidas=[comida1, comida2, comida3, comida4, comida5];
-const peliculas = [pel1, pel2, pel3, pel4, pel5, pel6, pel7, pel8, pel9, pel10, pel11, pel12, pel13, pel14, pel15, pel16, pel17, pel18, pel19, pel20, pel21, pel22, pel23, pel24, pel25, pel26, pel27, pel28, pel29, pel30, pel31, pel32, pel33, pel34, pel35, pel36];
-/* Se llama la funcion para guardar variables en el storage */
-llenarLocalStorage();
-/* localStorage.clear(); */
-/* Se crea un listado de categorias */
-let listaCategorias = [
+let pelis=[];/* Array que se llena con peliculas del ls */
+/* Se crea un array de categorias */
+
+let listaCarrito=[];
+const listaCategorias = [
   "Todas las Categorias",
   "Accion",
   "Aventura",
@@ -21,15 +18,43 @@ let listaCategorias = [
   "Infantil",
   "Terror",
 ];
-/* Mediante la funcion filtrarCategoria se filtran las peliculas del storage que coincidan con atributo genero escogido */
-let buscaCategoria=function filtrarCategoria(arr, filtro) {
-  const filtrado = arr.filter((el) => {
-    return el.genero==filtro;
+/* Pantalla de espera */
+/* Llamamos a la funcion consumir json y guardar en el localStorage */
+consumirPeliculas();
+
+/* Evento para los botones */
+function botonesComprar(){
+  const arrayBotones = document.querySelectorAll(".btn_comprar");
+  arrayBotones.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      let pelis=peliculaDesdeLS("peliculasLS");
+      pel = pelis.filter((el) => el.id== btn.id);
+      console.log(pel[0]);
+      console.log("Añadiste al carrito la pelicula: "+ pel[0].nombre);
+      Toastify({
+  
+        text: `Añadiste la pelicula: ${pel[0].nombre}`,
+        position: "bottom-left",
+        className: "mi-toastify",
+        offset: {
+          x: -30, // Ajusta el desplazamiento horizontal
+          y: 20, // Ajusta el desplazamiento vertical
+        },
+        duration: 3000,   
+        }).showToast();
+        contenedorCarrito.appendChild(crearItemCompra(pel[0]));
+        listaCarrito.push(pel[0]);
+        console.log("Lista carrito total:");
+        console.log(listaCarrito);
+        botonesComprar();
+console.log(listaCarrito.length);
+
+       
+    });
   });
-  return filtrado;
+  
 }
-/*Se recorre la lista para generar los li de categorias */
-for (const cate of listaCategorias) {
+for (const cate of listaCategorias) {/*Se recorre la lista para generar los li de categorias */
   const item = document.createElement("li");
   const categoria = document.createElement('a');
   categoria.textContent=cate;
@@ -40,67 +65,91 @@ for (const cate of listaCategorias) {
      categoria.onclick=()=>
      {
       contenedor.innerHTML="";
-       boton.innerText=cate;
-       for (const peli of buscaCategoria(peliculaDesdeLS('pelis'), cate)) {
+      const image=imagenCarga();
+      contenedor.appendChild(image);
+      boton.innerText=cate;
+      for (const peli of buscaCategoria(peliculaDesdeLS('peliculasLS'), cate)) {
                    /* Aqui va el friltrado por genero */
-       contenedor.appendChild(crearContenedor(peli));
+                              
+        carga(true,peli, image);
+       
        }
-      boton.innerText=="Todas las Categorias"? peliculaDesdeLS('pelis').forEach(peli => { 
-          contenedor.appendChild(crearContenedor(peli))}):"";
-          contenedor.innerHTML==""?contenedor.innerHTML=`<h3>lo sentimos, no hay peliculas de esta Categoria</h3>`: console.log("con peliculas"); 
-     }  
+       switch(boton.innerText){
+        case "Comedia":
+          contenedor.removeChild(image);
+          contenedor.innerHTML=`<h3>lo sentimos, no hay peliculas de esta Categoria</h3>`;
+          break;
+          case "Todas las Categorias":
+            contenedor.removeChild(image);
+            peliculaDesdeLS('peliculasLS').forEach(peli => { 
+              contenedor.appendChild(crearContenedor(peli))});
+              botonesComprar();
+       }
+
+     } 
+     
 }
 
 
 
-/* funcion para llenar el storage */
-function llenarLocalStorage(){
- localStorage.setItem('pelis', JSON.stringify(peliculas));/* Se convierte el listado de objetos a String */
- localStorage.setItem('food', JSON.stringify(comidas));
-} 
-/* Funcion para crear card de peliculas */
-function crearContenedor(dato){
-  const productArticle = document.createElement('article');
-  productArticle.innerHTML=
-  `<div class="card d-flex flex-fill" >
-  <img src=" ${dato.img}" alt="${dato.nombre}">
-  <h3>${dato.nombre}</h3>
-  <p>Precio: $${dato.precio} </p>
-  <p>Categoria: ${dato.genero} </p>
-    <div class="card-action">
-      <button>Comprar</button>
-    </div>
-</div>`;
-return productArticle;
-}
-  /* Funcion que retorna objetos que coinciden con la busqueda */
-  let busca=function filtrarPorCampo(arr, filtro) {
-    const filtrado = arr.filter((el) => {
-      return el.nombre.includes(filtro);
-    });
-    return filtrado;
-  }
-/* Para mostrar todas las peliculas */
-const peliculaDesdeLS=(llave)=>JSON.parse(localStorage.getItem(llave));/* Se llama cada lista */
-console.log(peliculaDesdeLS('pelis'));
-peliculaDesdeLS('pelis').forEach(peli => { 
+console.log(peliculaDesdeLS('peliculasLS'));
+peliculaDesdeLS('peliculasLS').forEach(peli => { 
   contenedor.appendChild(crearContenedor(peli));
-})
+});
 
-/* Evento click para busqueda de pelicula */
+
+/* Funcion que retorna objetos que coinciden con la busqueda */
+let busca=function filtrarPorCampo(arr, filtro) {
+  const filtrado = arr.filter((el) => {
+    return el.nombre.includes(filtro);
+  });
+  return filtrado;
+}
+
+
+
+
+
+
+/* EJECUCION DEL PROGRAMA */
+
+/* Evento click para busqueda de pelicula  por nombre*/
 buscar.onclick= (e)=>{
 
   e.preventDefault();
   contenedor.innerHTML="";/* Se eliminan los cards del contenedor para emular la recarga de la pagina por busqueda */
 
-  console.log(busca(peliculaDesdeLS('pelis'), inputBusqueda.value).length);
-  for(peli of busca(peliculaDesdeLS('pelis'), inputBusqueda.value)){/* Se los objetos filtrados para agregarlos como hijos del div principal */
-    contenedor.appendChild(crearContenedor(peli));
 
-  }
+  for(pel of busca(peliculaDesdeLS('peliculasLS'), inputBusqueda.value)){/* Se los objetos filtrados para agregarlos como hijos del div principal */
+  /* contenedor.appendChild(crearContenedor(pel)); */
+  /* carga(true,pel, imagenCarga()); */
+  contenedor.appendChild(crearContenedor(pel));
+
+  
+}
+
+  /* Para agregar y mostrar toastify */
+  botonesComprar(); 
   /* Condición para determinar si hay películas como resultado de la búsqueda */
   contenedor.innerHTML==""?contenedor.innerHTML=`<h3>lo sentimos, no hay peliculas</h3>`: console.log("con peliculas");   
-  console.log(f(peliculaDesdeLS('pelis'), inputBusqueda.value));
+ 
 
 
 }
+
+
+/*  */
+/* EJECUCIÓN DEL CÓDIGO */
+
+/* botonesComprar();
+if (contenedorCarrito!=""){
+  const btnEliminarItem= document.querySelectorAll(".carrito-eliminar");
+  console.log(btnEliminarItem.id );
+  btnEliminarItem.forEach((btn)=>{
+    btn.addEventListener("click", () => {
+      
+      console.log("tomaaa");
+  
+  });
+  });
+}; */
